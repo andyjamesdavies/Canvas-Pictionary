8cc87457fd87794f9e76da479ac665aa54a6231b
Jonathan Holmes <yoni.holmes@gmail.com>
2011-08-15 14:16:47 +0100

    Trying to start afresh

--- src/server/app.js
+++
@@ -1,6 +0,0 @@
-var server = require('./server.js')
-    players = require('./players.js'),
-    teams = require('./teams.js'),
-    srcDir = './../../src'; 
-
-server.start();
--- tests/first_test.js
+++
@@ -1,6 +0,0 @@
-TestCase("first", {
-	"test that a test works": function(){
-		assertTrue(false);
-	},
-
-});
